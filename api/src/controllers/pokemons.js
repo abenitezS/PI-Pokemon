40b9df089e62d17b214228da3716adfaa40fa898
Alicia Benítez Samaniego <benitez.ali@gmail.com>
2022-07-21 14:18:49 -0300

    mejorar con promisall

--- api/src/controllers/pokemons.js
+++ api/src/controllers/pokemons.js
@@ -8,78 +8,87 @@ const {Pokemon, Type} = require("../db.js");
 function crearObj(data) {
     return {
         id: data.id,
-        name: data.name,
+        name: data.name,// 4 a 150 https://hualao.com/es/what-is-the-name-of-the-longest-pokemon.html
         hp: data.stats[0].base_stat,
-        attack: data.stats[1].base_stat,
-        defense: data.stats[2].base_stat,
-        speed: data.stats[5].base_stat,
-        height: data.height,
-        weight: data.weight,
+        attack: data.stats[1].base_stat,//5-190
+        defense: data.stats[2].base_stat,//5 a 250
+        speed: data.stats[5].base_stat,//5-180
+        height: data.height,//0..15 metros (decimales?)
+        weight: data.weight,///1 a 1000  (decimales?)   https://pokemon.fandom.com/es/wiki/Lista_de_Pok%C3%A9mon_por_peso
         image: data.sprites.other.home.front_default,
-        types: data.types.map(t => t.type.name)
+        types: data.types.map(t => t.type.name)   //1 o 2 tipos permitidos https://pokemon.fandom.com/es/wiki/Lista_de_Pok%C3%A9mon_por_tipo
     }
 }
 
+
 router.get('/',async (req, res, next) => {
-        let {name} = req.query;
-        let pokemonsCreated = await Pokemon.findAll({
-            include: {
-                model: Type,
-                attributes: ["name"]
-            }
-        });
-
-        if (!name || name === undefined) {
-            try {
-               const {data} = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=40")           
-                const urls = [];
-                data.results?.map(p => urls.push(p.url));
-                
-                let  pokemons = [];
-                for (let url of urls) {
-                    let {data} = await axios.get(url);
-                    pokemons.push(crearObj(data));
-                }
-
-                if (pokemonsCreated.length > 0) {
-                    pokemonsCreated = pokemonsCreated.map(p => {
-                        return p.dataValues
-                    });
-                    pokemons = pokemons.concat(pokemonsCreated);
-                }
-
-                return res.json(pokemons)
-            } catch (error) {
-                next(error);
-            }
-        }else{ //Si me pasan ?name=...
-            try {
-                if (pokemonsCreated.length === 0) {
-                    let {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-                    return res.json(crearObj(data));
-                } else {
-                    let find = await Pokemon.findOne({
-                        where: {
-                            name: name.toLowerCase()
-                        },
-                        include: {
-                            model: Type,
-                            attributes: ["name"]
-                        }
-                    })
-                    if (find) {
-                        return res.json(find);
-                    }
-                    let {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-                    return res.json(crearObj(data));
-                }
-            } catch (error) {
-                return res.status(404).json({msj: "No se encuentra el pokemon solicitado"});
-            }
+    let {name} = req.query;
+    let pokemonsCreated = await Pokemon.findAll({
+        include: {
+            model: Type,
+            attributes: ["name"]
         }
+    });
+
+    if (!name || name === undefined) {
+        try {
+            let url ="https://pokeapi.co/api/v2/pokemon/"
+            let  pokemons = []
+
+           while(url!=null && pokemons.length<40) 
+           {
+            const {data} = await axios.get(url)           
+            let auxPokemones= data.results?.map(e => {
+                              return {name: e.name, url: e.url} //obtengo las urls y los nombres
+                             } );
+                pokemons.push(...auxPokemones);
+                url=data.next
+            } 
+
+            let pokes = await Promise.all(pokemons.map(async e=>{
+                let pokemon = await axios.get(e.url);
+                return crearObj(pokemon.data);
+            }))
+
+            if (pokemonsCreated.length > 0) {
+                pokemonsCreated = pokemonsCreated.map(p => {
+                    return p.dataValues
+                });
+                pokes = pokes.concat(pokemonsCreated);
+            }
+
+            return res.json(pokes)
+        } catch (error) {
+            next(error);
+        }
+    }else{ //Si me pasan ?name=...
+        try {
+            if (pokemonsCreated.length === 0) {
+                let {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+                return res.json(crearObj(data));
+            } else {
+                let find = await Pokemon.findOne({
+                    where: {
+                        name: name.toLowerCase()
+                    },
+                    include: {
+                        model: Type,
+                        attributes: ["name"]
+                    }
+                })
+                if (find) {
+                    return res.json(find);
+                }
+                let {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+                return res.json(crearObj(data));
+            }
+        } catch (error) {
+            return res.status(404).json({msj: "No se encuentra el pokemon solicitado"});
+        }
+    }
 
 })
-router.get ( '/:id' , async (req, res, next) => {
+router.get ( '/:id' , async (req, res) => {
         let {id} = req.params; 
         
         if (id.includes("-")) {
