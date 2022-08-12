dc1abbe78b6d367c5680ca7dfc11d7a59dca7b1d
Alicia Benítez Samaniego <benitez.ali@gmail.com>
2022-08-12 01:39:22 -0300

    ok

--- client/src/components/NaBar/NavBar.js
+++ client/src/components/NaBar/NavBar.js
@@ -4,13 +4,12 @@ import titulo from '../../img/tituloPokemon.png'
 import { Switch, Route, NavLink } from 'react-router-dom'
 import style from './NavBar.module.css'
 
-
 export default function NavBar() {
  
     return (
         <div className={style.contenedorNavbar}>
             <NavLink to="/pokemons">
-                <img src={titulo} alt="titulo" />
+                <img className={style.image} src={titulo} alt="titulo" />
             </NavLink>
             <Switch>
                 <Route exact path={"/home"}>
